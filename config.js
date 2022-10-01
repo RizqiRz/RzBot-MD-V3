29f88857a5d6134c93fbc6cb95d9d1181715041c
Rizqi06 <compoterindo@gmail.com>
2022-10-01 21:01:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6283865752867']
+global.ownernomer = "6283865752867"
+global.premium = ['6283865752867']
+global.packname = 'RzBot-MD'
+global.author = 'RZ06'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
